942aaf6a13d5d9040eb5d031d01b3c422d09173f
Robin PARISI <parisi.robin@gmail.com>
2020-04-21 00:03:30 +0200

    Slugify project name to avoid conflict with parsing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const datefns = require('date-fns')
 const https = require('https')
 const axios = require('axios')
+const slugify = require('slugify')
 
 const Backup = require('./src/Backup')
 const Storage = require('./src/Storage')
@@ -19,40 +20,47 @@ try {
 ;(async function () {
   // iterate over all projects to backup
     helpers.asyncForEach(config.projects, async (project) => {
-      helpers.info('processing...', project.slug)
+      const slug = slugify(project.slug, {
+        replacement: '_',
+        remove: /[*+~.()'"!:@-]/g,
+        lower: true,
+        strict: true
+      })
+
+      helpers.info('processing...', slug)
 
       // get all backups from S3
       const s3 = new Storage(config.awsAccessKeyId, config.awsSecretAccessKey, config.awsBucket)
-      let filesList = await s3.listAllFilesForProject(project.slug)
+      let filesList = await s3.listAllFilesForProject(slug)
 
       // check for backup
       const toProcess = manager.checkForBackup(project, filesList)
 
       if (toProcess.length > 0) {
-        helpers.info(`prepare ${toProcess}`, project.slug)
+        helpers.info(`prepare ${toProcess}`, slug)
 
         const formatedDate = datefns.format(new Date(), 'yyyyMMdd_HHmmss')
-        const filename = project.slug + '-' + formatedDate
+        const filename = slug + '-' + formatedDate
 
         // backup, upload to S3 and clean old backups
         try {
           const backup = await Backup.exec(project.path, filename)
-          helpers.info('run pliz backup command', project.slug)
+          helpers.info('run pliz backup command', slug)
 
           // sync upload to spare network
           await helpers.asyncForEach(toProcess, async (frequency) => {
-            const s3Filename = `${project.slug}-${frequency}-${formatedDate}.tar.gz`
-            await s3.uploadFile(backup, project.slug, s3Filename)
-            helpers.info(`upload ${s3Filename} to s3`, project.slug)
+            const s3Filename = `${slug}-${frequency}-${formatedDate}.tar.gz`
+            await s3.uploadFile(backup, slug, s3Filename)
+            helpers.info(`upload ${s3Filename} to s3`, slug)
           })
 
           // clean old backups
-          filesList = await s3.listAllFilesForProject(project.slug)
+          filesList = await s3.listAllFilesForProject(slug)
           const toDelete = manager.checkForBackupToDelete(project, filesList)
 
           // sync delete
           await helpers.asyncForEach(toDelete, async (key) => {
-            helpers.info(`delete ${key} from s3`, project.slug)
+            helpers.info(`delete ${key} from s3`, slug)
             await s3.deleteFile(key)
           })
 
@@ -73,7 +81,7 @@ try {
         }
       } else {
         https.get(project.pingUrl)
-        helpers.info('no backup to perform', project.slug)
+        helpers.info('no backup to perform', slug)
       }
     })
   })()
